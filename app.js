a9b000d4e757004d7c86e0c4051a6cd0b962a0a0
585jeremy <j.wirtz1512@gmail.com>
2026-03-31 12:41:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const DISCORD_INVITE_URL = "https://discord.gg/Y8HNFPtxkE";
 
 const SERVER_JOIN_CODE = "pbe6gy";
 const SERVER_SINGLE_API_URL = `https://servers-frontend.fivem.net/api/servers/single/${SERVER_JOIN_CODE}`;
-const MAP_IMAGE_URL = "https://www.gtabase.com/igallery/maps/gta-5-map-roadmap-labels-1920.jpg";
+const MAP_IMAGE_URL = "map.jpg";
 const MAP_BASE_SIZE = 980;
 const MAP_MIN_SCALE = 1;
 const MAP_MAX_SCALE = 2.8;
