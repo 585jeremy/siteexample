347fe587e253bbb7c5ff9703d652025e41bd57b4
585jeremy <j.wirtz1512@gmail.com>
2026-04-04 23:57:41 +0200

    Staff preview

--- staff-site/staff-gate-config.js
+++ staff-site/staff-gate-config.js
@@ -2,6 +2,13 @@ window.STAFF_GATE_CONFIG = {
   storageKey: "sgcnr_staff_gate_v2",
   accounts: [
     {
+      username: "stf-8Q4M-27JK",
+      password: "Jeremy!",
+      displayName: "Jeremy",
+      clearance: "General Staff",
+      issuedBy: "Management Team",
+    },
+    {
       username: "staff-preview",
       password: "ChangeMe-2026",
       displayName: "Staff Preview",
